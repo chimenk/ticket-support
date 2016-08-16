92a498f31528a740fb43527adf8c2b3deb99e2a5
Yandi Kurniawan <yandikurniawan@gmail.com>
2016-08-16 11:20:46 +0700

    Init Project

---
+++ app/Http/Controllers/TicketsController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Category;
+use App\Ticket;
+use Auth;
+
+class TicketsController extends Controller
+{
+    public function create()
+    {
+        $categories = Category::all();
+
+        return view('tickets.create', compact('categories'));
+    }
+
+    public function store(Request $req)
+    {
+        $this->validate($req, [
+            'title'     => 'required',
+            'category'  => 'required',
+            'priority'  => 'required',
+            'message'   => 'required'
+        ]);
+
+        $ticket = new Ticket([
+            'title'     =>  $req->input('title'),
+            'user_id'   =>  Auth::user()->id,
+            'ticket_id' =>  strtoupper(str_random(10)),
+            'category_id'   =>  $req->input('category'),
+            'priority'  =>  $req->input('priority'),
+            'message'   =>  $req->input('message'),
+            'status'    =>  "Open"
+        ]);
+
+        $ticket->save();
+
+        // Send mail to user inform about this created ticket
+        Mail::to(Auth::user()->email)->send(new TicketCreated($ticket));
+
+        return redirect()->back()->with('status', 'A ticket with ID: #$ticket->ticket_id has been opened.');
+    }
+
+    public function userTickets()
+    {
+        $tickets = Ticket::where('user_id', Auth::user()->id)->paginate(10);
+        $categories = Category::all();
+
+        return view('tickets.user_tickets', compact('tickets', 'categories'));
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| This file is where you may define all of the routes that are handled
+| by your application. Just tell Laravel the URIs it should respond
+| to using a Closure or controller method. Build something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
+
+Route::get('new_ticket', 'TicketsController@create');
+Route::post('new_ticket', 'TicketsController@store');
+Route::get('my_tickets', 'TicketsController@userTickets');
+Route::get('tickets/{ticket_id}', 'TicketsController@show');
